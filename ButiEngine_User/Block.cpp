9daad442dbe0c60cfe2454f3e1be03e28a3b268b
butibuti <butikouki@gmail.com>
2022-07-23 23:50:24 +0900

    Blockの色変化の復活

--- ButiEngine_User/Block.cpp
+++ ButiEngine_User/Block.cpp
@@ -4,19 +4,19 @@
 
 void ButiEngine::Block::OnUpdate()
 {
-    //m_current += m_pase;
+    m_current += m_pase;
 
-    //if (m_current >= 1.0f) {
-    //    m_current = 1.0f;
-    //    m_pase *= -1;
-    //}
-    //else
-    //    if (m_current <= 0.0f) {
-    //        m_current = 0.0f;
-    //        m_pase *= -1;
-    //    }
+    if (m_current >= 1.0f) {
+        m_current = 1.0f;
+        m_pase *= -1;
+    }
+    else
+        if (m_current <= 0.0f) {
+            m_current = 0.0f;
+            m_pase *= -1;
+        }
 
-    //shp_lightBuffer->Get().lightDir = wkp_mapComponent.lock()->GetMapStartColor() * (1 - current) + wkp_mapComponent.lock()->GetMapEndColor() * current;
+    m_vlp_objectInformation->Get().color = m_vwp_mapComponent.lock()->GetMapStartColor() * (1 - m_current) + m_vwp_mapComponent.lock()->GetMapEndColor() * m_current;
 }
 
 void ButiEngine::Block::OnSet()
@@ -29,27 +29,27 @@ void ButiEngine::Block::OnRemove()
 
 void ButiEngine::Block::OnShowUI()
 {
-    //GUI::BulletText("CurrentGlowPower");
-    //GUI::DragFloat("##currentGlowPower", current, 0.01f, 0, 1);
-    //GUI::BulletText("GlowPowerPase");
-    //GUI::DragFloat("##tGlowPowerPase", pase, 0.01f, 0, 1);
+    GUI::BulletText("CurrentGlowPower");
+    GUI::DragFloat("##currentGlowPower", m_current, 0.01f, 0, 1);
+    GUI::BulletText("GlowPowerPase");
+    GUI::DragFloat("##tGlowPowerPase", m_pase, 0.01f, 0, 1);
 }
 
 void ButiEngine::Block::Start()
 {
-    //shp_lightBuffer = gameObject.lock()->GetGameComponent<MeshDrawComponent_Static>()->GetCBuffer<LightVariable>("LightBuffer");
-    //auto pos = gameObject.lock()->transform->GetWorldPosition();
-    //current = (sin(MathHelper::Dot(Vector2(pos.x, pos.z), Vector2(1.9898 + pos.y, 8.233))) * 437.645);
-    //current -= (int)current;
+    m_vlp_objectInformation = gameObject.lock()->GetGameComponent<MeshDrawComponent>()->GetCBuffer<ButiRendering::ObjectInformation>();
+    auto pos = gameObject.lock()->transform->GetWorldPosition();
+    m_current = (sin(Vector2(pos.x, pos.z).Dot(Vector2(1.9898 + pos.y, 8.233))) * 437.645);
+    m_current -= static_cast<int>(m_current);
 
-    //auto map = GetManager().lock()->GetGameObject("Map");
-    //wkp_mapComponent = map.lock()->GetGameComponent<Map>();
+    auto map = GetManager().lock()->GetGameObject("Map");
+    m_vwp_mapComponent = map.lock()->GetGameComponent<Map>();
 }
 
 ButiEngine::Value_ptr<ButiEngine::GameComponent> ButiEngine::Block::Clone()
 {
     auto output = ObjectFactory::Create<Block>();
-    //output->wkp_mapComponent = wkp_mapComponent;
+    output->m_vwp_mapComponent = m_vwp_mapComponent;
     output->m_pase = m_pase;
     output->m_current = m_current;
     return output;
--- ButiEngine_User/Block.h
+++ ButiEngine_User/Block.h
@@ -2,32 +2,32 @@
 #include"Header/GameComponentHeader.h"
 namespace ButiEngine {
 
-	class Map;
+class Map;
 
-	class Block :public GameComponent
+class Block :public GameComponent
+{
+public:
+	std::string GetGameComponentName()const override {
+		return "Block";
+	}
+	void OnUpdate()override;
+	void OnSet()override;
+	void OnRemove()override;
+	void OnShowUI()override;
+	void Start()override;
+	Value_ptr<GameComponent> Clone()override;
+	template<class Archive>
+	void serialize(Archive& archive)
 	{
-	public:
-		std::string GetGameComponentName()const override {
-			return "Block";
-		}
-		void OnUpdate()override;
-		void OnSet()override;
-		void OnRemove()override;
-		void OnShowUI()override;
-		void Start()override;
-		Value_ptr<GameComponent> Clone()override;
-		template<class Archive>
-		void serialize(Archive& archive)
-		{
-			archive(isActive);
-			archive(m_pase);
-			archive(m_current);
-		}
-	private:
-		Value_weak_ptr<Map> m_vwp_mapComponent;
-		float m_pase;
-		float m_current = 0.5f;
-		//std::shared_ptr<CBuffer<LightVariable>> shp_lightBuffer;
-	};
+		archive(isActive);
+		archive(m_pase);
+		archive(m_current);
+	}
+private:
+	Value_weak_ptr<Map> m_vwp_mapComponent;
+	float m_pase;
+	float m_current = 0.5f;
+	Value_ptr<ButiRendering::CBuffer<ButiRendering::ObjectInformation>> m_vlp_objectInformation;
+};
 }
 BUTI_REGIST_GAMECOMPONENT(Block, true)
